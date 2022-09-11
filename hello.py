7bce879f8b90385b950554931f9f6f4403db3cfa
Osita Amaechi <ositabamaechi@gmail.com>
2022-09-11 06:47:58 +0100

    This is my first git commit for Python

---
+++ hello.py
@@ -0,0 +1,30 @@
+# firstname = " osita "
+# lastname = "amaechi"
+# fullname = f"{firstname} {lastname}"
+# print(fullname)
+# print(fullname.title())
+# print(fullname.upper())
+# print (f"Hello {fullname.title()} would you like to study python language today? ")
+# print('Albert Einstein said "A person who never made a \nmistake never tried anything new"')
+# name = 'Albert Einstien'
+# message = ('Albert Einstein said "A person who never made a \nmistake never tried anything new"')
+# print (f"{name} {message}")
+# print (firstname.title())
+# print(firstname.strip())
+# print(4 + 4)
+# print(4 * 2)
+# print(12 - 4)
+# print(16/2)
+# x = ('8')
+# # print (f"My favorite number is {x}")
+
+# cars = ['audi', 'bmw', 'toyota','peugout']
+# print(cars)
+# for car in cars:
+#     print(car.title())
+cars = ['audi', 'bmw', 'toyota','peugout']
+for car in cars:
+    if car == 'bmw':
+        print(car.upper())
+    else:
+        print(car.title())    
